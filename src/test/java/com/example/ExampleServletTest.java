653ba7007b2b17482405e37e4a09ac1ab134760b
Andriy Zhdanov <azhdanov@gmail.com>
2012-12-06 00:45:46 +0100

    altas-create-refapp-plugin

---
+++ src/test/java/com/example/ExampleServletTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link com.example.ExampleServlet}
+ */
+public class ExampleServletTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
