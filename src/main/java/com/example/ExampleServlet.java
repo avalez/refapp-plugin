4323d7d362c818b56d07f0fc3d96261ad20383c1
Andriy Zhdanov <azhdanov@gmail.com>
2012-12-06 01:04:14 +0100

    render vm template

--- src/main/java/com/example/ExampleServlet.java
+++ src/main/java/com/example/ExampleServlet.java
@@ -4,14 +4,28 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.atlassian.templaterenderer.TemplateRenderer;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExampleServlet extends HttpServlet
 {
+	private static final long serialVersionUID = -1070578716351115012L;
+	private static final String TEMPLATE = "templates/example.vm";
+
+	private final TemplateRenderer renderer;
+
+    public ExampleServlet(TemplateRenderer renderer) {
+		this.renderer = renderer;
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException
     {
-        res.getWriter().write("Hello world");
-        res.getWriter().close();
+        final Map<String, Object> context = new HashMap<String, Object>();
+        renderer.render(TEMPLATE, context, res.getWriter());
     }
 }
